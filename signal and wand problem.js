f69e398f4bd3e7d0e56bbbefce86b1e456c894be
ZackAkil <zackakil94@gmail.com>
2016-03-03 17:14:00 +0000

    initial commit

---
+++ signal and wand problem.js
@@ -0,0 +1,67 @@
+function timeToWaitAtSignal(){
+
+	if(Math.floor((Math.random() * 2))){
+		return 0; // if its green
+	}else{
+		return Math.floor((Math.random() * 80) + 1); // if its red
+	}
+
+}
+
+function averageTimeWaiting(loops){
+
+	var total = 0;
+
+	for (var i = 0; i < loops; i++) {
+		total += timeToWaitAtSignal();
+	};
+	return total/loops;
+
+}
+
+function averageClavin(signals, wands, wandThreshold, loops){
+
+	var total = 0;
+
+	for (var i = 0; i < loops; i++) {
+		
+		var journeyTime = 0;
+
+		var wandsToUse = wands;
+		for (var j = 0; j < signals; j++) {
+
+			var time = timeToWaitAtSignal();
+			if((time > wandThreshold)&&(wandsToUse > 0)){
+				time = 0;
+				wandsToUse --;
+			}
+			journeyTime += time;
+		};
+		total += journeyTime;
+	};
+
+	return total/loops;
+}
+
+function optimalWandUse(signals, wands, loops){
+
+	var results = "";
+	var minWandTreshold;
+	var minAvergeTime = 80;
+
+	for(var i = 0; i < 80; i++){
+
+		var avg = averageClavin(signals,wands,i,loops);
+		if(avg < minAvergeTime){
+			minAvergeTime = avg;
+			minWandTreshold = i;
+		}
+		results += avg.toString() + "\n";
+	}
+
+	return results + "optimal wand threshold : "
+	 + minWandTreshold.toString()
+	  + " with average time of "
+	   + minAvergeTime.toString();
+
+}
