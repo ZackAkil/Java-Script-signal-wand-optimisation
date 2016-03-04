542066d1c239d55751aef43f18b57f57efb13199
ZackAkil <zackakil94@gmail.com>
2016-03-04 20:49:35 +0000

    refactor journey code into own function

--- signal and wand problem.js
+++ signal and wand problem.js
@@ -7,32 +7,36 @@ function timeToWaitAtSignal(){
 	}
 }
 
-function averageCalvin(signals, wands, wandThreshold, loops){
+function performJourney(signals, wands, wandThreshold){
+
+	var time =0;
+	var journeyTime = 0;
+	var wandsToUse = wands;
+
+	for (var j = 0; j < signals; j++) {
+
+		time = timeToWaitAtSignal();
+
+		if(time != 0){ // add 0 if its a green light
+			if((signals - j)<=wandsToUse){ // 
+				time = 0;
+				wandsToUse --;
+			}else if((time >= wandThreshold)&&(wandsToUse > 0)){
+				time = 0;
+				wandsToUse --;	
+			}
+		}
+		journeyTime += time;
+	}
+	return journeyTime;
+}
+
+function averageJourneyTime(signals, wands, wandThreshold, loops){
 
 	var total = 0;
-	var journeyTime,wandsToUse,time;
 
 	for (var i = 0; i < loops; i++) {
-		
-		journeyTime = 0;
-		wandsToUse = wands;
-
-		for (var j = 0; j < signals; j++) {
-
-			time = timeToWaitAtSignal();
-
-			if(time != 0){ // add 0 if its a green light
-				if((signals - j)<=wandsToUse){ // 
-					time = 0;
-					wandsToUse --;
-				}else if((time >= wandThreshold)&&(wandsToUse > 0)){
-					time = 0;
-					wandsToUse --;	
-				}
-			}
-			journeyTime += time;
-		}
-		total += journeyTime;
+		total += performJourney(signals,wands,wandThreshold);
 	}
 	return total/loops;
 }
@@ -45,7 +49,7 @@ function optimalWandUse(signals, wands, loops){
 
 	for(var i = 0; i < 80; i++){
 
-		var avg = averageCalvin(signals,wands,i,loops);
+		var avg = averageJourneyTime(signals,wands,i,loops);
 		if(avg < minAvergeTime){
 			minAvergeTime = avg;
 			minWandTreshold = i;
